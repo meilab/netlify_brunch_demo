d99635f20ce4bd9fc4fbf12ac05174ac34e9dc2c
Wang Yi <linucywang@aliyun.com>
2017-12-23 06:02:55 +0800

    debug

--- brunch-config.js
+++ brunch-config.js
@@ -1,5 +1,55 @@
-// See http://brunch.io for documentation.
-exports.files = {
-  javascripts: {joinTo: 'app.js'},
-  stylesheets: {joinTo: 'app.css'}
+module.exports = {
+  config: {
+    paths: {
+      watched: ["app"],
+      // Where to compile files to
+      public: "./public"
+    },
+    files: {
+      javascripts: {
+        joinTo: "js/app.js"
+      },
+      stylesheets: {
+        joinTo: "css/app.css",
+        order: {
+          after: ["static/css/app.css"] // concat app.css last
+        }
+      }
+    },
+    conventions: {
+      // This option sets where we should place non-css and non-js assets in.
+      // By default, we set this to "/static/assets". Files in this directory
+      // will be copied to `paths.public`, which is "priv/static" by default.
+      assets: /^(app\/assets)/
+    },
+    plugins: {
+      babel: {
+        // Do not use ES6 compiler in vendor code and elm code
+        ignore: [/static\/vendor/, /elm.js$/]
+      },
+      elmBrunch: {
+        mainModules: ["src/Main.elm"],
+        outputFolder: "static/js/",
+        outputFile: "elm.js",
+        makeParameters : ['--debug','--warn']
+      }
+    },
+    modules: {
+      autoRequire: {
+        "js/app.js": ["app/js/app"]
+      }
+    },
+    npm: {
+      enabled: true
+    },
+    overrides: {
+      production: {
+        plugins: {
+          elmBrunch: {
+            makeParameters: []
+          }
+        }
+      }
+    }
+  }
 };
