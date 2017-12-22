48a5455e01ce26b53e2a18d25aaf08b489d1a9f7
Wang Yi <linucywang@aliyun.com>
2017-12-23 07:54:42 +0800

    debug

--- brunch-config.js
+++ brunch-config.js
@@ -16,18 +16,6 @@ module.exports = {
         }
       }
     },
-    conventions: {
-      // This option sets where we should place non-css and non-js assets in.
-      // By default, we set this to "/static/assets". Files in this directory
-      // will be copied to `paths.public`, which is "priv/static" by default.
-      assets: /^(app\/assets)/
-    },
-    plugins: {
-      babel: {
-        // Do not use ES6 compiler in vendor code and elm code
-        ignore: [/app\/vendor/, /elm.js$/]
-      }
-    },
     npm: {
       enabled: true
     }
